4692c0f11fb7693d1eb379a6390ea4e76daa28e8
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-10 13:52:12 +0300

    feature:add functionality to the custom model class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,23 @@
 export class Quote {
+
+    constructor
+    (
+     public id:number,  
+     public author:string,
+     public text:string, 
+     public writer:string,
+     public publicationDate:Date,
+     public upVote: number,
+     public downVote:number
+     )
+    {
+        this.id= id;
+        this.author=author;
+        this.text=text;
+        this.writer=writer;
+        this.publicationDate=publicationDate;
+        this.upVote=upVote;
+        this.downVote=downVote;
+
+    }
 }
