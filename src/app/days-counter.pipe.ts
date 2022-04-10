167cb8ddda40a67c1635f09ddee01666faaa4354
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-10 16:06:41 +0300

    add fuctionality of days counter pipe

--- src/app/days-counter.pipe.ts
+++ src/app/days-counter.pipe.ts
@@ -4,9 +4,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'daysCounter'
 })
 export class DaysCounterPipe implements PipeTransform {
+  transform(value: any): number {
+    let today:Date = new Date(); 
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    var dateDifference = Math.abs(todayWithNoTime - value) 
+
+    const secondsInDay = 86400; 
+
+    var dateDifferenceSeconds = dateDifference*0.001;
+
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+    if (todayWithNoTime > value ){
+      return (dateCounter);
+    }else{
+      return 0;
+    }
   }
 
 }
