503ad337bb0d08e06d263aebda19255e21a1c785
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-10 14:11:27 +0300

    add data to display-quote component

--- src/app/display-quote/display-quote.component.ts
+++ src/app/display-quote/display-quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+
 
 @Component({
   selector: 'app-display-quote',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-quote.component.css']
 })
 export class DisplayQuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote(1,'William W.', "'You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.'",'John Doe',new Date(2020,3,14),0,0),
+    new Quote(2,'Marilyn Monroe', "'Imperfection is beauty, madness is genius and it's better to be absolutely ridiculous than absolutely boring.'",'Jane Doe',new Date(2020,3,14),3,0),
+    new Quote(3,'Neil Gaiman',"'Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten.'",'Anon Hacker',new Date(2022,3,8),0,0)
+  
+  ];
 
   constructor() { }
 
