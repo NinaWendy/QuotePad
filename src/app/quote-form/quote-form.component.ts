b0500d0b6a525a16fdd0e61402d0b0c5402fb7e4
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-04-10 15:54:25 +0300

    add push functionality

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0,"","","",new Date(),0,0, false);
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit(): void {
